93661e8de3a9063c073f57a95392010a160767c9
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:44:11 +0530

    Update __speechtotext.py

--- LovelyRobot/modules/__speechtotext.py
+++ LovelyRobot/modules/__speechtotext.py
@@ -1,4 +1,4 @@
-from MashaRoBot import telethn as tbot
+from LovelyRobot import telethn as tbot
 import os
 import urllib.request
 from datetime import datetime
@@ -11,8 +11,8 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from MashaRoBot import *
-from MashaRoBot.events import register
+from LovelyRobot import *
+from LovelyRobot.events import register
 
 
 async def is_register_admin(chat, user):
