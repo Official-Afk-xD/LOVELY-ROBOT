26b4c50cce3c4121f8e95829f449909c261dc765
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:30:29 +0530

    Update forceSubscribe_sql.py

--- LovelyRobot/modules/sql_extended/forceSubscribe_sql.py
+++ LovelyRobot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from MashaRoBot.modules.sql import BASE, SESSION
+from LovelyRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
