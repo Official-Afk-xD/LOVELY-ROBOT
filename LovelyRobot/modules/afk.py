d2d764ead094e9a1bb75f0c8735d491b06a21e6c
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:52:43 +0530

    Update afk.py

--- LovelyRobot/modules/afk.py
+++ LovelyRobot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.disable import (
+from LovelyRobot import dispatcher
+from LovelyRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from MashaRoBot.modules.sql import afk_sql as sql
-from MashaRoBot.modules.users import get_user_id
+from LovelyRobot.modules.sql import afk_sql as sql
+from LovelyRobot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
