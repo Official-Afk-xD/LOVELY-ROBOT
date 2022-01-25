2b52a9c9e523cb048efc67d50eae513686639874
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:20:58 +0530

    Update reporting_sql.py

--- LovelyRobot/modules/sql/reporting_sql.py
+++ LovelyRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from MashaRoBot.modules.sql import BASE, SESSION
+from LovelyRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
