c6c659f350a549c1f6bc32ab3694415eadba6fe1
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:47:58 +0530

    Update _pyrocovid.py

--- LovelyRobot/modules/_pyrocovid.py
+++ LovelyRobot/modules/_pyrocovid.py
@@ -1,7 +1,7 @@
-from MashaRoBot import pbot as app
-from MashaRoBot.pyrogramee.errors import capture_err
-from MashaRoBot.pyrogramee.json_prettify import json_prettify
-from MashaRoBot.pyrogramee.fetch import fetch
+from LovelyRobot import pbot as app
+from LovelyRobot.pyrogramee.errors import capture_err
+from LovelyRobot.pyrogramee.json_prettify import json_prettify
+from LovelyRobot.pyrogramee.fetch import fetch
 from pyrogram import filters
 
 
