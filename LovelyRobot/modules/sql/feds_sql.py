eb48033d768af459397df4a466055dbb49ff9b5a
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:14:42 +0530

    Update feds_sql.py

--- LovelyRobot/modules/sql/feds_sql.py
+++ LovelyRobot/modules/sql/feds_sql.py
@@ -3,8 +3,8 @@ import threading
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.sql import BASE, SESSION
+from LovelyRobot import dispatcher
+from LovelyRobot.modules.sql import BASE, SESSION
 
 
 class Federations(BASE):
