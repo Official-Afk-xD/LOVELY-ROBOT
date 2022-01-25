c02894a3aae54d7bebd9341342809b907699d4e1
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:16:39 +0530

    Update kukiai_sql.py

--- LovelyRobot/modules/sql/kukiai_sql.py
+++ LovelyRobot/modules/sql/kukiai_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from sqlalchemy import Column, String
-from MashaRoBot.modules.sql import BASE, SESSION
+from LovelyRobot.modules.sql import BASE, SESSION
 
 class KukiChats(BASE):
     __tablename__ = "kuki_chats"
