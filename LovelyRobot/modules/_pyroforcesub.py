180c2127b8ce3958eb06321878405f89a0da2cf9
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-25 16:49:24 +0530

    Update _pyroforcesub.py

--- LovelyRobot/modules/_pyroforcesub.py
+++ LovelyRobot/modules/_pyroforcesub.py
@@ -1,4 +1,4 @@
-# credits @InukaAsith, @Mr_dark_prince
+# credits @log_afk, @evil_xD_boy
 
 import logging
 import time
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from MashaRoBot import DRAGONS as SUDO_USERS
-from MashaRoBot import pbot
-from MashaRoBot.modules.sql_extended import forceSubscribe_sql as sql
+from LovelyRobot import DRAGONS as SUDO_USERS
+from LovelyRobot import pbot
+from LovelyRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
