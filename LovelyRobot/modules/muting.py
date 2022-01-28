aca57fc3fac2b720683fdc6bc9877e4b25dc3045
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 00:42:58 +0530

    Update muting.py

--- LovelyRobot/modules/muting.py
+++ LovelyRobot/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from MashaRoBot import LOGGER, TIGERS, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import (
+from LovelyRobot import LOGGER, TIGERS, dispatcher
+from LovelyRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from MashaRoBot.modules.helper_funcs.extraction import (
+from LovelyRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from MashaRoBot.modules.helper_funcs.string_handling import extract_time
-from MashaRoBot.modules.log_channel import loggable
+from LovelyRobot.modules.helper_funcs.string_handling import extract_time
+from LovelyRobot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
