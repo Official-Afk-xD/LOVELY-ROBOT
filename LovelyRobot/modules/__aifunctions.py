712b744fecf1f56e1d3fc7b2011dd113dd679b62
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 01:41:24 +0530

    Update __aifunctions.py

--- LovelyRobot/modules/__aifunctions.py
+++ LovelyRobot/modules/__aifunctions.py
@@ -37,7 +37,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Masha"
+   pro = "My name is Lovely"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
