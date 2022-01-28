72b966a958ce31a41a1b72b10b969dd0faf600b6
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 01:28:20 +0530

    Update dbcleanup.py

--- LovelyRobot/modules/dbcleanup.py
+++ LovelyRobot/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import MashaRoBot.modules.sql.global_bans_sql as gban_sql
-import MashaRoBot.modules.sql.users_sql as user_sql
-from MashaRoBot import DEV_USERS, OWNER_ID, dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
+import LovelyRobot.modules.sql.global_bans_sql as gban_sql
+import LovelyRobot.modules.sql.users_sql as user_sql
+from LovelyRobot import DEV_USERS, OWNER_ID, dispatcher
+from LovelyRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
