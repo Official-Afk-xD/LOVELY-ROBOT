e8da1fe69112a9dc3777089d02e83805adf06913
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 01:26:18 +0530

    Update dev.py

--- LovelyRobot/modules/dev.py
+++ LovelyRobot/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import MashaRoBot
+import LovelyRobot
 
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
+from LovelyRobot import dispatcher
+from LovelyRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
@@ -21,9 +21,9 @@ def allow_groups(update: Update, context: CallbackContext):
         update.effective_message.reply_text(f"Current state: {MashaRoBot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
-        MashaRoBot.ALLOW_CHATS = True
+        LovelyRobot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
-        MashaRoBot.ALLOW_CHATS = False
+        LovelyRobot.ALLOW_CHATS = False
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
