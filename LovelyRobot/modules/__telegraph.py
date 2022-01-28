a62a40b089a0142b3bfbe3f28b460ddc6353cb62
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 03:04:54 +0530

    Update __telegraph.py

--- LovelyRobot/modules/__telegraph.py
+++ LovelyRobot/modules/__telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-Log afk = "Lovely"
+Logafk = "Lovely"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=darkprince)
 auth_url = r["auth_url"]
