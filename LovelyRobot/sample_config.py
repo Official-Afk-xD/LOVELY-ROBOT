38b606115f45c5a5053f4054865458ecf4c4216c
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 02:15:14 +0530

    Update sample_config.py

--- LovelyRobot/sample_config.py
+++ LovelyRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/MashaRoBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/LovelyRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,11 +17,11 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 2091736526  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "log_afk"
+    SUPPORT_CHAT = "Blaze_support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001577398383
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
         -1001190806654
