2b99517a6b796c6491c8adff8c4db579f8968a9c
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 00:26:45 +0530

    Update speed_test.py

--- LovelyRobot/modules/speed_test.py
+++ LovelyRobot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from MashaRoBot import DEV_USERS, dispatcher
-from MashaRoBot.modules.disable import DisableAbleCommandHandler
-from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
+from LovelyRobot import DEV_USERS, dispatcher
+from LovelyRobot.modules.disable import DisableAbleCommandHandler
+from LovelyRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
