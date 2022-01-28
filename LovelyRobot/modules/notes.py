df2be55e605445fe59b7a0e7baf636d81ff83eb2
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 00:41:23 +0530

    Update notes.py

--- LovelyRobot/modules/notes.py
+++ LovelyRobot/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import MashaRoBot.modules.sql.notes_sql as sql
-from MashaRoBot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from MashaRoBot.modules.disable import DisableAbleCommandHandler
-from MashaRoBot.modules.helper_funcs.handlers import MessageHandlerChecker
-from MashaRoBot.modules.helper_funcs.chat_status import user_admin, connection_status
-from MashaRoBot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from MashaRoBot.modules.helper_funcs.msg_types import get_note_type
-from MashaRoBot.modules.helper_funcs.string_handling import (
+import LovelyRobot.modules.sql.notes_sql as sql
+from LovelyRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from LovelyRobot.modules.disable import DisableAbleCommandHandler
+from LovelyRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from LovelyRobot.modules.helper_funcs.chat_status import user_admin, connection_status
+from LovelyRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from LovelyRobot.modules.helper_funcs.msg_types import get_note_type
+from LovelyRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (
