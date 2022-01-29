bb889c8ba4e7d1b390cd086239f8179ec133b497
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 15:38:44 +0530

    Update logomaker.py

--- LovelyRobot/modules/logomaker.py
+++ LovelyRobot/modules/logomaker.py
@@ -4,7 +4,7 @@ import glob
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterPhotos
 from LovelyRobot.events import register
-from LovelyRobot import telethn as tbot, ubot2
+
 
 
 def mediainfo(media):
