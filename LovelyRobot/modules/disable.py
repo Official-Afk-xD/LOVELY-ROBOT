9019e912c16546089bb1663d13efcf760f9c9c2a
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 11:35:55 +0530

    Update disable.py

--- LovelyRobot/modules/disable.py
+++ LovelyRobot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     ❍ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "ᴅɪsᴀʙʟᴇ👎"
+    __mod_name__ = "Disable 👎"
 
 else:
     DisableAbleCommandHandler = CommandHandler
