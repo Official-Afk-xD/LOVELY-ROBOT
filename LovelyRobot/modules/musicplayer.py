16f2cf4bf0745b28972f035f50637491a7319fa3
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 17:41:42 +0530

    Create musicplayer.py

---
+++ LovelyRobot/modules/musicplayer.py
@@ -0,0 +1,22 @@
+__mod_name__ = "Music Player🎧"
+
+__help__ = """
+=>> *Song Playing* 🎧 
+❍ /play <song name> - play song you requested
+❍ /playlist - Show now playing list
+❍ /current - Show now playing
+❍ /song <song name> - download songs you want quickly
+❍ /search <query> - search videos on youtube with details
+❍ /video <song name> - download videos you want quickly
+=>> *Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+❍ /userbotjoin - invite assistant to your chat
+❍ /admincache - Refresh admin list
+=>>   *Use*
+1) Add @HyperAssistant in your group to admin with all permission
+2) Assistant Auto Join Send /play Command First Tme By Owner
+"""
