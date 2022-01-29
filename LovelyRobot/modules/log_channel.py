f69e3ba144be574a94f27419b460d7fc75d02e1e
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 12:39:26 +0530

    Update log_channel.py

--- LovelyRobot/modules/log_channel.py
+++ LovelyRobot/modules/log_channel.py
@@ -204,7 +204,7 @@ Setting the log channel is done by:
 ❍ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "ᴄʜᴀɴɴᴇʟ📢"
+    __mod_name__ = "Channel 📢"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
