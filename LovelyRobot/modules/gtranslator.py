547b3286ebf9ed645da95b2e40725189e7cff570
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 12:36:39 +0530

    Update gtranslator.py

--- LovelyRobot/modules/gtranslator.py
+++ LovelyRobot/modules/gtranslator.py
@@ -109,6 +109,6 @@ TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "G-Tʀᴀɴs🌐"
+__mod_name__ = "G-Trans 🌐"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
