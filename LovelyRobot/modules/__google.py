7d87654fce4ad301944f04e854ca2632336add62
Øғғɪᴄɪᴀʟ Ⱡᴏɢ [₳ғᴋ] <97865824+Official-Afk-xD@users.noreply.github.com>
2022-01-29 10:58:40 +0530

    Update __google.py

--- LovelyRobot/modules/__google.py
+++ LovelyRobot/modules/__google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Gᴏᴏɢʟᴇ🤳"
+__mod_name__ = "Google 🤳"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
